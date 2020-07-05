1e849c239ae1b01451f64b02fa74d4c4250ef1ae
install-b <645256685@qq.com>
2020-07-05 14:26:22 +0800

    change folder

---
+++ Classes/MultiProxyObjectExcute.swift
@@ -0,0 +1,35 @@
+//
+//  MultiProxyObjectExcute.swift
+//  DelegateCenter
+//
+//  Created by Shangen Zhang on 2020/7/5.
+//
+
+import Foundation
+
+
+/// 组合模式拓展
+public protocol MultiProxyObjectExcute {
+    /// 指定协议类型
+    associatedtype MPT: NSObjectProtocol
+    /// 多代理链表
+    var proxyObject: MultiProxyObject<MPT> { get }
+}
+
+/// 添加移除遍历通用操作
+public extension MultiProxyObjectExcute {
+    @discardableResult
+    func add(delegate: MPT) -> Bool {
+        proxyObject.add(delegate: delegate)
+    }
+    
+    @discardableResult
+    func remove(delegate: MPT) -> Bool {
+        proxyObject.remove(delegate: delegate)
+    }
+
+    func enumerateDelegate(using block:((_ delegate: MPT, _ stop: UnsafeMutablePointer<ObjCBool>) -> Void)) {
+        proxyObject.enumerateDelegate(using: block)
+    }
+}
+
