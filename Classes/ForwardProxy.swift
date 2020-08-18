cebd62b47b999108378cfbae1aec822d38e58a9a
shangenzhang <645256685@qq.com>
2020-08-18 19:15:03 +0800

    Objc

--- Classes/ForwardProxy.swift
+++ Classes/ForwardProxy.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 
-/// 代理转发
-open class ForwardProxy<T: NSObject>: WeakProxyArray {
+/// 代理转发 只能转发 Objc代理
+open class ForwardProxy<T: NSObjectProtocol>: WeakProxyArray {
     @nonobjc public convenience init(delegates: [T]) {
         self.init(__delegates: delegates)
     }
